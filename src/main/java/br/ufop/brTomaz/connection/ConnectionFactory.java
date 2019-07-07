7b63725d277d0aeacb9e390c7bd0c4f15aea04f8
Lucas Pereira Miranda <permiranda.lucas@gmail.com>
2019-07-06 22:09:54 -0300

    atualiza~cao

--- src/main/java/br/ufop/brTomaz/connection/ConnectionFactory.java
+++ src/main/java/br/ufop/brTomaz/connection/ConnectionFactory.java
@@ -20,7 +20,7 @@ public class ConnectionFactory {
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
     private static final String URL = "jdbc:mysql://localhost:3306/db_carona?useTimezone=true&serverTimezone=UTC";
     private static final String USER = "root";
-    private static final String PASS = "YES";
+    private static final String PASS = "";
     
     public static Connection getConnection(){
         
